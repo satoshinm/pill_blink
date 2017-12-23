134f88446bce7039d1a2dee5599e627a9d8bb08a
Satoshi N. M <snmatsutake@yahoo.co.jp>
2017-12-22 23:02:39 -0800

    Change to use registers directly instead of function calls

--- bare_metal/pill_blink.c
+++ bare_metal/pill_blink.c
@@ -2,13 +2,16 @@
 #include <libopencm3/stm32/gpio.h>
 
 int main(void) {
-    rcc_periph_clock_enable(RCC_GPIOC);
-    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+    RCC_APB2ENR |= RCC_APB2ENR_IOPCEN;
+
+    GPIOC_CRH = (GPIO_CNF_OUTPUT_PUSHPULL << (((13 - 8) * 4) + 2));
+    GPIOC_CRH |= (GPIO_MODE_OUTPUT_2_MHZ << ((13 - 8) * 4));
 
     while(1) {
-        gpio_set(GPIOC, GPIO13);
+        GPIOC_BSRR = GPIO13;
         for (int i = 0; i < 1000000; ++i) __asm__("nop");
-        gpio_clear(GPIOC, GPIO13);
+
+        GPIOC_BRR = GPIO13;
         for (int i = 0; i <  500000; ++i) __asm__("nop");
     }
 }
